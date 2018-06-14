e40c8ccb41208afdf340f160ea01b4592d40bdd3
sandiasolutions <danieleaton747@gmail.com>
2018-06-14 16:15:58 -0600

    began development for Scon class

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # new-sconsdoc
 The New SconsDoc
+
---
+++ php/classes/Scon.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: deaton747
+ * Date: 6/14/2018
+ * Time: 3:45 PM
+ */
+
+namespace classes;
+
+
+class Scon
+{
+ public function __construct($sconId, $firstName, $lastName, $middleInitial,$netId, $email,$phoneNumber, $startDate, $adminStatus)
+ {
+ }
+}
