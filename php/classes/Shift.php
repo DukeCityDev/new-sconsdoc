1fa0697be0398b794a2c596e920cbcaae58e7dd1
SandiaSolutions <danieleaton747@gmail.com>
2018-07-02 19:44:24 -0600

    Finished Untested version of Accessor and Mutator methods

--- php/classes/Shift.php
+++ php/classes/Shift.php
@@ -39,14 +39,14 @@ require_once(dirname(__DIR__) . "/util/Util.php");
 class Shift
 {
     private $shiftId;
-    private $sconId;
+    private $sconNetId;
     private $podId;
     private $shiftPlanId;
     private $startDate;
     private $endDate;
     private $available;
 
-    public function __construct(?int $shiftId, int $sconId, int $podId, int $shiftPlanId, \DateTime $startDate, \DateTime $endDate, boolean $available){
+    public function __construct(?int $shiftId, ?string $sconNetId, int $podId, int $shiftPlanId, \DateTime $startDate, \DateTime $endDate, boolean $available){
 
     }
 
@@ -70,8 +70,106 @@ class Shift
         return $this->shiftId;
     }
 
-    public function setPodId( int $pod){
+    public function setSconNetId( ?string $sconNetId):void{
+        if(!is_null($sconNetId)){
+            if(!is_string($sconNetId)){
+                throw new \TypeError("Scon Net Id is Not a String");
+            } else if(strlen($sconNetId) > 20){
+                throw new \OutOfRangeException("Scon Net Id is Too Long (More than 20 characters)");
+            } else if(strlen($sconNetId) < 3 ){
+                throw new \OutOfRangeException("Scon Net Id is Too Short (Less than 3 characters");
+            }
+        }
 
+        $this->sconNetId = $sconNetId;
     }
+
+    public function getSconNetId():?string{
+        return $this->sconNetId;
+    }
+
+    public function setPodId(int $podId){
+        if(!is_int($podId)){
+            throw new \InvalidArgumentException("Pod Id is Invalid: Not An Integer");
+        } else if($podId < 0){
+            throw new \InvalidArgumentException("Pod ID is Invalid: Negative Integer");
+        } else if($podId >= 4294967296){
+            throw new \OutOfBoundsException("Pod ID is Invalid: Maximum INT(10) Size, assign more bytes to Pod Id");
+        }
+
+        $this->podId = $podId;
+    }
+
+    public function getPodId():int{
+        return $this->getPodId();
+    }
+
+    public function setShiftPlanId(int $shiftPlanId):void{
+        if(!is_int($shiftPlanId)){
+            throw new \InvalidArgumentException("ShiftPlanId is Invalid: Not An Integer");
+        } else if($shiftPlanId < 0){
+            throw new \InvalidArgumentException("ShiftPlanId is Invalid: Negative Integer");
+        } else if($shiftPlanId >= 4294967296){
+            throw new \OutOfBoundsException("ShiftPlanId is Invalid: Maximum INT(10) Size, assign more bytes to Pod Id");
+        }
+
+        $this->shiftPlanId = $shiftPlanId;
+    }
+
+    public function getShiftPlanId():int{
+        return $this->shiftPlanId;
+    }
+
+    public function setStartDate( ?\DateTime $date): void
+    {
+        if(is_null($date)){
+            $date = new \DateTime("now");
+        }
+        if(!Util::verifyDate($date)){
+            throw new \InvalidArgumentException("Date Time is not Valid");
+        }
+        $this->startDate = $date;
+    }
+
+    public function getStartDate(): \DateTime
+    {
+        return $this->startDate;
+    }
+
+    public function setEndDate( ?\DateTime $date): void
+    {
+        if(is_null($date)){
+            $date = new \DateTime("now");
+        }
+        if(!Util::verifyDate($date)){
+            throw new \InvalidArgumentException("Date Time is not Valid");
+        }
+        $this->endDate = $date;
+    }
+
+    public function getEndDate(): \DateTime
+    {
+        return $this->endDate;
+    }
+
+    /**
+     * @param mixed $available
+     */
+    public function setAvailable(boolean $available)
+    {
+        if(is_null($this->sconNetId && !$available)){
+            throw new \InvalidArgumentException("Shift cannot be unavailable if it has no assigned Scon");
+        }
+        $this->available = $available;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAvailable():boolean
+    {
+        return $this->available;
+    }
+
 
 }
